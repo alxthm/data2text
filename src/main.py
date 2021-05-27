c3039f321fd4801684c98db3bfcb06399f9b67f8
Alexandre Thomas <al.thomas@criteo.com>
2021-05-27 10:17:24 +0200

    add utils for model summary, fix logging of src files, clean up config

--- src/main.py
+++ src/main.py
@@ -1,11 +1,8 @@
 import datetime
-import datetime
-import random
 import sys
 from pathlib import Path
 
 import mlflow
-import numpy as np
 import torch
 import torch.optim
 from omegaconf import OmegaConf
@@ -15,145 +12,123 @@ from tqdm import tqdm
 
 from src.data.webnlg import prepare_data, tokenizer
 from src.model.cycle_cvae import CycleCVAE
-from src.utils import WarningsFilter
-
-tb_writer: SummaryWriter = None
-run_name = ""
-
-
-def train(conf, device, dataloader_train, dataloader_val, dataloader_test, vocab):
-    print("Preparing model...")
-    model = CycleCVAE(
-        text_vocab=vocab["text"],
-        ent_vocab=vocab["entity"],
-        rel_vocab=vocab["relation"],
-        tokenizer=tokenizer,  # todo: obtain correctly instead of importing
-        tot_epoch=conf.main.epoch,
-        dim_h=conf.main.dim_h,
-        dim_z=conf.g2t.vae_dim,
-        enc_lstm_layers=conf.g2t.enc_lstm_layers,
-        n_head=conf.g2t.nhead,
-        head_dim=conf.g2t.head_dim,
-        emb_dropout=conf.g2t.emb_drop,
-        attn_drop=conf.g2t.attn_drop,
-        drop=conf.g2t.drop,
-        n_layers_gat=conf.g2t.n_layers_gat,
-        g2t_lr=conf.g2t.lr,
-        g2t_weight_decay=conf.g2t.weight_decay,
-        beam_size=conf.g2t.beam_size,
-        beam_max_len=conf.g2t.beam_max_len,
-        length_penalty=conf.g2t.length_penalty,
-        t2g_drop=conf.t2g.drop,
-        t2g_lr=conf.t2g.lr,
-        t2g_weight_decay=conf.t2g.weight_decay,
-        gradient_clip_val=conf.main.grad_clip,
-        run_name=run_name,
-    )
-    model.to(device)
-
-    global_step = 0
-    for ep in range(0, conf.main.epoch):
-        # training epoch
-        for batch in tqdm(dataloader_train, desc=f"[train][ep{ep}]"):
-            metrics = model.training_step(batch, global_step=global_step)
-            model.on_train_step_end(tb_writer, global_step)
-            mlflow.log_metrics(metrics, step=global_step)
-            for k, v in metrics.items():
-                tb_writer.add_scalar(k, v, global_step=global_step)
-            global_step += 1
-
-        # validation epoch
-        model.eval()
-        with torch.no_grad():
-            model.on_eval_epoch_start()
-            for batch in tqdm(dataloader_val, desc=f"[val][ep{ep}]"):
-                model.eval_step(batch)
-            model.on_eval_epoch_end("val", tb_writer, global_step)
-
-            if ep % 15 == 0 and conf.main.save_checkpoints:
-                # save model checkpoint
-                torch.save(model.state_dict(), f"/tmp/{run_name}_model_ep{ep}.pt")
-                mlflow.log_artifact(
-                    f"/tmp/{run_name}_model_ep{ep}.pt", f"model_ep{ep}.pt"
-                )
-
-    # test
-    torch.save(model.state_dict(), f"/tmp/{run_name}_model_final.pt")
-    mlflow.log_artifact(f"/tmp/{run_name}_model_final.pt", "model_final.pt")
-    model.eval()
-    with torch.no_grad():
-        model.on_eval_epoch_start()
-        for batch in tqdm(dataloader_test, desc="[test]"):
-            model.eval_step(batch)
-        model.on_eval_epoch_end("test", tb_writer, global_step)
+from src.utils import (
+    WarningsFilter,
+    seed_everything,
+    mlflow_log_src_and_config,
+    ModelSummary,
+)
 
 
 def main(timestamp: str):
-    device = torch.device(0) if torch.cuda.is_available() else torch.device("cpu")
-
     # Load config
     project_dir = Path(__file__).resolve().parents[1]
     conf = OmegaConf.load(project_dir / "conf/config.yaml")
     print(OmegaConf.to_yaml(conf))
 
     # seed everything
-    random.seed(conf.main.seed)
-    torch.manual_seed(conf.main.seed)
-    np.random.seed(conf.main.seed)
-    torch.cuda.manual_seed_all(conf.main.seed)
-    torch.backends.cudnn.deterministic = True
-    torch.backends.cudnn.benchmark = False
+    device = torch.device(0) if torch.cuda.is_available() else torch.device("cpu")
+    seed_everything(conf.seed)
 
     mlflow.set_tracking_uri("https://mlflow.par.prod.crto.in/")
     mlflow.set_experiment("al.thomas_data_2_text")
     # todo: batch updates to speed up training
     #  https://www.mlflow.org/docs/latest/python_api/mlflow.tracking.html#mlflow.tracking.MlflowClient.log_batch
-    global run_name
-    run_name = f"{timestamp}-{conf.main.mode}"
-    global tb_writer
+    run_name = f"{timestamp}-{conf.mode}"
     tb_writer = SummaryWriter(log_dir=str(project_dir / f"models/{run_name}"))
     print(f"run_name: {run_name}\n")
 
     with mlflow.start_run(run_name=run_name):
-        for k1 in conf.keys():
-            for k2 in conf[k1].keys():
-                mlflow.log_param(f"{k1}_{k2}", conf[k1][k2])
-        # save source files and vocab
-        for f in (project_dir / "src").rglob("*.py"):
-            mlflow.log_artifact(str(f), f"code/{f}")
+        mlflow_log_src_and_config(conf, project_dir)
 
         # load data
         dataset_train, dataset_val, dataset_test, vocab, collate_fn = prepare_data(
             data_dir=project_dir / "data", device=device
         )
-        batch_size = conf.g2t.batch_size
-        assert batch_size == conf.t2g.batch_size
         dataloader_train = DataLoader(
             dataset_train,
-            batch_size=batch_size,
+            batch_size=conf.batch_size,
             shuffle=True,
             collate_fn=collate_fn,
         )
         dataloader_val = DataLoader(
             dataset_val,
-            batch_size=batch_size,
+            batch_size=conf.batch_size,
             collate_fn=collate_fn,
         )
         dataloader_test = DataLoader(
             dataset_test,
-            batch_size=batch_size,
+            batch_size=conf.batch_size,
             collate_fn=collate_fn,
         )
 
-        # train
-        train(
-            conf=conf,
-            device=device,
-            dataloader_train=dataloader_train,
-            dataloader_val=dataloader_val,
-            dataloader_test=dataloader_test,
-            vocab=vocab,
+        # prepare model
+        model = CycleCVAE(
+            text_vocab=vocab["text"],
+            ent_vocab=vocab["entity"],
+            rel_vocab=vocab["relation"],
+            tokenizer=tokenizer,  # todo: obtain correctly instead of importing
+            tot_epoch=conf.epoch,
+            dim_h=conf.dim_h,
+            dim_z=conf.g2t.dim_z,
+            enc_lstm_layers=conf.g2t.enc_lstm_layers,
+            n_head=conf.g2t.n_head,
+            head_dim=conf.g2t.head_dim,
+            emb_dropout=conf.g2t.emb_drop,
+            attn_drop=conf.g2t.attn_drop,
+            drop=conf.g2t.drop,
+            n_layers_gat=conf.g2t.n_layers_gat,
+            g2t_lr=conf.g2t.lr,
+            g2t_weight_decay=conf.g2t.weight_decay,
+            beam_size=conf.g2t.beam_size,
+            beam_max_len=conf.g2t.beam_max_len,
+            length_penalty=conf.g2t.length_penalty,
+            t2g_drop=conf.t2g.drop,
+            t2g_lr=conf.t2g.lr,
+            t2g_weight_decay=conf.t2g.weight_decay,
+            gradient_clip_val=conf.grad_clip,
+            run_name=run_name,
         )
+        model.to(device)
+        summary = ModelSummary(model, mode="top")
+        print(summary)
+
+        # train
+        global_step = 0
+        for ep in range(0, conf.epoch):
+            # training epoch
+            for batch in tqdm(dataloader_train, desc=f"[train][ep{ep}]"):
+                metrics = model.training_step(batch, global_step=global_step)
+                model.on_train_step_end(tb_writer, global_step)
+                mlflow.log_metrics(metrics, step=global_step)
+                for k, v in metrics.items():
+                    tb_writer.add_scalar(k, v, global_step=global_step)
+                global_step += 1
+
+            # validation epoch
+            model.eval()
+            with torch.no_grad():
+                model.on_eval_epoch_start()
+                for batch in tqdm(dataloader_val, desc=f"[val][ep{ep}]"):
+                    model.eval_step(batch)
+                model.on_eval_epoch_end("val", tb_writer, global_step)
+
+                if ep % 15 == 0 and conf.save_checkpoints:
+                    # save model checkpoint
+                    torch.save(model.state_dict(), f"/tmp/{run_name}_model_ep{ep}.pt")
+                    mlflow.log_artifact(
+                        f"/tmp/{run_name}_model_ep{ep}.pt", f"model_ep{ep}.pt"
+                    )
+
+        # test
+        torch.save(model.state_dict(), f"/tmp/{run_name}_model_final.pt")
+        mlflow.log_artifact(f"/tmp/{run_name}_model_final.pt", "model_final.pt")
+        model.eval()
+        with torch.no_grad():
+            model.on_eval_epoch_start()
+            for batch in tqdm(dataloader_test, desc="[test]"):
+                model.eval_step(batch)
+            model.on_eval_epoch_end("test", tb_writer, global_step)
 
 
 if __name__ == "__main__":
--- src/utils.py
+++ src/utils.py
@@ -1,3 +1,15 @@
+from collections import OrderedDict
+from pathlib import Path
+from typing import Union, List, Tuple, Dict
+
+import mlflow
+import numpy as np
+import random
+
+import torch
+from torch import nn
+
+
 class WarningsFilter:
     def __init__(self, stream):
         """
@@ -26,3 +38,273 @@ class WarningsFilter:
 
     def flush(self):
         self.stream.flush()
+
+
+def seed_everything(seed: int):
+    random.seed(seed)
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    torch.backends.cudnn.deterministic = True
+    torch.backends.cudnn.benchmark = False
+
+
+def mlflow_log_src_and_config(conf, project_dir: Path):
+    # log hyperparameters in config
+    for k1 in conf.keys():
+        if hasattr(conf[k1], "keys"):
+            for k2 in conf[k1].keys():
+                mlflow.log_param(f"{k1}.{k2}", conf[k1][k2])
+        else:
+            mlflow.log_param(f"{k1}", conf[k1])
+
+    # save source files
+    for f in (project_dir / "src").rglob("*.py"):
+        mlflow.log_artifact(str(f.relative_to(project_dir)), f"code/{f}")
+
+
+# from https://github.com/PyTorchLightning/pytorch-lightning/
+
+
+class LayerSummary(object):
+    """
+    Summary class for a single layer in a :class:`~pytorch_lightning.core.lightning.LightningModule`.
+    It collects the following information:
+    - Type of the layer (e.g. Linear, BatchNorm1d, ...)
+    - Input shape
+    - Output shape
+    - Number of parameters
+    The input and output shapes are only known after the example input array was
+    passed through the model.
+    Example::
+        >>> model = torch.nn.Conv2d(3, 8, 3)
+        >>> summary = LayerSummary(model)
+        >>> summary.num_parameters
+        224
+        >>> summary.layer_type
+        'Conv2d'
+        >>> output = model(torch.rand(1, 3, 5, 5))
+        >>> summary.in_size
+        [1, 3, 5, 5]
+        >>> summary.out_size
+        [1, 8, 3, 3]
+    Args:
+        module: A module to summarize
+    """
+
+    def __init__(self, module: nn.Module):
+        super().__init__()
+        self._module = module
+        self._in_size = None
+        self._out_size = None
+
+    @property
+    def in_size(self) -> Union[str, List]:
+        return self._in_size or "?"
+
+    @property
+    def out_size(self) -> Union[str, List]:
+        return self._out_size or "?"
+
+    @property
+    def layer_type(self) -> str:
+        """ Returns the class name of the module. """
+        return str(self._module.__class__.__name__)
+
+    @property
+    def num_parameters(self) -> int:
+        """ Returns the number of parameters in this module. """
+        return sum(np.prod(p.shape) for p in self._module.parameters())
+
+
+class ModelSummary(object):
+    """
+    Generates a summary of all layers in a :class:`~pytorch_lightning.core.lightning.LightningModule`.
+    Args:
+        model: The model to summarize (also referred to as the root module)
+        mode: Can be one of
+             - `top` (default): only the top-level modules will be recorded (the children of the root module)
+             - `full`: summarizes all layers and their submodules in the root module
+    The string representation of this summary prints a table with columns containing
+    the name, type and number of parameters for each layer.
+    The root module may also have an attribute ``example_input_array`` as shown in the example below.
+    If present, the root module will be called with it as input to determine the
+    intermediate input- and output shapes of all layers. Supported are tensors and
+    nested lists and tuples of tensors. All other types of inputs will be skipped and show as `?`
+    in the summary table. The summary will also display `?` for layers not used in the forward pass.
+    Example::
+        >>> import pytorch_lightning as pl
+        >>> class LitModel(pl.LightningModule):
+        ...
+        ...     def __init__(self):
+        ...         super().__init__()
+        ...         self.net = nn.Sequential(nn.Linear(256, 512), nn.BatchNorm1d(512))
+        ...         self.example_input_array = torch.zeros(10, 256)  # optional
+        ...
+        ...     def forward(self, x):
+        ...         return self.net(x)
+        ...
+        >>> model = LitModel()
+        >>> ModelSummary(model, mode='top')  # doctest: +NORMALIZE_WHITESPACE
+          | Name | Type       | Params | In sizes  | Out sizes
+        ------------------------------------------------------------
+        0 | net  | Sequential | 132 K  | [10, 256] | [10, 512]
+        ------------------------------------------------------------
+        132 K     Trainable params
+        0         Non-trainable params
+        132 K     Total params
+        0.530     Total estimated model params size (MB)
+        >>> ModelSummary(model, mode='full')  # doctest: +NORMALIZE_WHITESPACE
+          | Name  | Type        | Params | In sizes  | Out sizes
+        --------------------------------------------------------------
+        0 | net   | Sequential  | 132 K  | [10, 256] | [10, 512]
+        1 | net.0 | Linear      | 131 K  | [10, 256] | [10, 512]
+        2 | net.1 | BatchNorm1d | 1.0 K    | [10, 512] | [10, 512]
+        --------------------------------------------------------------
+        132 K     Trainable params
+        0         Non-trainable params
+        132 K     Total params
+        0.530     Total estimated model params size (MB)
+    """
+
+    def __init__(self, model, mode: str):
+        self._model = model
+        self._mode = mode
+        self._layer_summary = self.summarize()
+
+    @property
+    def named_modules(self) -> List[Tuple[str, nn.Module]]:
+        if self._mode == "full":
+            mods = self._model.named_modules()
+            mods = list(mods)[1:]  # do not include root module (LightningModule)
+        elif self._mode == "top":
+            # the children are the top-level modules
+            mods = self._model.named_children()
+        else:
+            mods = []
+        return list(mods)
+
+    @property
+    def layer_names(self) -> List[str]:
+        return list(self._layer_summary.keys())
+
+    @property
+    def layer_types(self) -> List[str]:
+        return [layer.layer_type for layer in self._layer_summary.values()]
+
+    @property
+    def param_nums(self) -> List[int]:
+        return [layer.num_parameters for layer in self._layer_summary.values()]
+
+    @property
+    def total_parameters(self) -> int:
+        return sum(p.numel() for p in self._model.parameters())
+
+    @property
+    def trainable_parameters(self) -> int:
+        return sum(p.numel() for p in self._model.parameters() if p.requires_grad)
+
+    def summarize(self) -> Dict[str, LayerSummary]:
+        summary = OrderedDict(
+            (name, LayerSummary(module)) for name, module in self.named_modules
+        )
+        return summary
+
+    def __str__(self):
+        """
+        Makes a summary listing with:
+        Layer Name, Layer Type, Number of Parameters
+        """
+        arrays = [
+            [" ", list(map(str, range(len(self._layer_summary))))],
+            ["Name", self.layer_names],
+            ["Type", self.layer_types],
+            ["Params", list(map(get_human_readable_count, self.param_nums))],
+        ]
+        total_parameters = self.total_parameters
+        trainable_parameters = self.trainable_parameters
+
+        return _format_summary_table(total_parameters, trainable_parameters, *arrays)
+
+    def __repr__(self):
+        return str(self)
+
+
+def get_human_readable_count(number: int) -> str:
+    """
+    Abbreviates an integer number with K, M, B, T for thousands, millions,
+    billions and trillions, respectively.
+    Examples:
+        >>> get_human_readable_count(123)
+        '123  '
+        >>> get_human_readable_count(1234)  # (one thousand)
+        '1.2 K'
+        >>> get_human_readable_count(2e6)   # (two million)
+        '2.0 M'
+        >>> get_human_readable_count(3e9)   # (three billion)
+        '3.0 B'
+        >>> get_human_readable_count(4e14)  # (four hundred trillion)
+        '400 T'
+        >>> get_human_readable_count(5e15)  # (more than trillion)
+        '5,000 T'
+    Args:
+        number: a positive integer number
+    Return:
+        A string formatted according to the pattern described above.
+    """
+    assert number >= 0
+    labels = [" ", "K", "M", "B", "T"]
+    num_digits = int(np.floor(np.log10(number)) + 1 if number > 0 else 1)
+    num_groups = int(np.ceil(num_digits / 3))
+    num_groups = min(num_groups, len(labels))  # don't abbreviate beyond trillions
+    shift = -3 * (num_groups - 1)
+    number = number * (10 ** shift)
+    index = num_groups - 1
+    if index < 1 or number >= 100:
+        return f"{int(number):,d} {labels[index]}"
+
+    return f"{number:,.1f} {labels[index]}"
+
+
+def _format_summary_table(
+    total_parameters: int, trainable_parameters: int, *cols
+) -> str:
+    """
+    Takes in a number of arrays, each specifying a column in
+    the summary table, and combines them all into one big
+    string defining the summary table that are nicely formatted.
+    """
+    n_rows = len(cols[0][1])
+    n_cols = 1 + len(cols)
+
+    # Get formatting width of each column
+    col_widths = []
+    for c in cols:
+        col_width = max(len(str(a)) for a in c[1]) if n_rows else 0
+        col_width = max(col_width, len(c[0]))  # minimum length is header length
+        col_widths.append(col_width)
+
+    # Formatting
+    s = "{:<{}}"
+    total_width = sum(col_widths) + 3 * n_cols
+    header = [s.format(c[0], l) for c, l in zip(cols, col_widths)]
+
+    # Summary = header + divider + Rest of table
+    summary = " | ".join(header) + "\n" + "-" * total_width
+    for i in range(n_rows):
+        line = []
+        for c, l in zip(cols, col_widths):
+            line.append(s.format(str(c[1][i]), l))
+        summary += "\n" + " | ".join(line)
+    summary += "\n" + "-" * total_width
+
+    summary += "\n" + s.format(get_human_readable_count(trainable_parameters), 10)
+    summary += "Trainable params"
+    summary += "\n" + s.format(
+        get_human_readable_count(total_parameters - trainable_parameters), 10
+    )
+    summary += "Non-trainable params"
+    summary += "\n" + s.format(get_human_readable_count(total_parameters), 10)
+    summary += "Total params"
+
+    return summary
